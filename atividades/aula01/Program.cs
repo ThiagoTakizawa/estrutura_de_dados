203208f2194518f6073d8c95f1e187e2ba0d6e30
Thiago Seiji Takizawa <433989@unoesc.lan>
2024-08-14 22:07:07 -0300

    primeiro projeto .net

---
+++ atividades/aula01/Program.cs
@@ -0,0 +1,8 @@
+﻿//declarando uma variável
+int number;
+//atribuindo valor
+number = 10;
+//declarando constante
+const int DAYS_IN_WEEK = 7;
+//imprimindo string concatenada
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias.");
