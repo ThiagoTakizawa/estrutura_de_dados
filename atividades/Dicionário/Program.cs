3de0232f9080b3b53ee8eee59f96d828b8a99a52
Thiago Seiji Takizawa <433989@unoesc.lan>
2024-09-25 22:02:41 -0300

    Aula 25/09

---
+++ atividades/Dicionário/Program.cs
@@ -0,0 +1,60 @@
+﻿//Diferente da HashTable , o Dictionary nos permite especificar o tipo de dado que será utilizado nos pares Key - Value
+//Isso nos fornece uma estrutura fortemente tipada
+
+using Dicionário;
+
+Dictionary<string, string> dictionary = new Dictionary<string, string>()
+{
+    {"Key 1", "Value 1"},
+    {"Key 2", "value 2"}
+};
+
+//Obtero valor do dictionary
+string val = dictionary["Key 1"];
+
+try
+{
+    val = dictionary["Key 3"];
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erro ao obter valor da Key 3");
+    Console.WriteLine(ex.Message);
+}
+
+//Verificando se a chave existe antes de acessar para evitar erros e prejuízos de execução
+if(dictionary.ContainsKey("Key 2"))
+{
+    
+}
+
+//Outra forma de obter o valor e evitar o erro
+dictionary.TryGetValue("Key 4", out string? valor);
+if(valor is not null)
+{
+    Console.WriteLine($"Chave 2: {dictionary["Key 4"]}");
+}
+
+//Podemos adicionar da mesma forma pela chave inexistente
+dictionary["Key 0"] = "Valor 0";
+
+//Percorrer o Dictionary
+foreach(KeyValuePair<string, string> kvp in dictionary)
+{
+    Console.WriteLine($"{kvp.Key}:{kvp.Value}");
+}
+
+//Utilizando tipos abstratos de dados com Dictionary
+
+Dictionary<int, Pessoa> dicPessoa = new Dictionary<int, Pessoa>();
+
+dicPessoa.Add(1, new Pessoa(){Id = 0, BirthDate = new DateTime(1984, 7, 5), Name = "Mauricio Roberto Gonzatto"});
+dicPessoa.Add(1, new Pessoa(){Id = 0, BirthDate = new DateTime(2001, 5, 16), Name = "Marco Antonio"});
+
+foreach(KeyValuePair<int, Pessoa> kvp in dicPessoa)
+{
+    Console.WriteLine(kvp.Key);
+    Console.WriteLine(kvp.Value.Name);
+    Console.WriteLine(kvp.Value.Id);
+    Console.WriteLine(kvp.Value.BirthDate);   
+}
---
+++ atividades/HashTable/Program.cs
@@ -0,0 +1,41 @@
+﻿//Exemplo de uma agenda telefônica
+
+using System.Collections;
+
+Hashtable phoneBook = new Hashtable()
+{
+    { "Thomazzi", "49-9961-0150" },     // "Chave", "Valor"
+    { "Mauricio Gonzatto", "49-99975-8575" },
+    {"Gabriel Bianchi", "49-99105-8904" },
+};
+
+//É possível adicionar elementos de diversas formas, pelo prórpio índice chave inexistente
+phoneBook["Thiago Padilha"] = "49-99176-8255";
+
+//Ou pelo método Add()
+phoneBook.Add("Marcos Henrique", "49-99202-6129");
+
+//Entretanto , a HashTable verifica se há duplicidade de chave e pode lançar uma Exception
+try
+{
+    phoneBook.Add("Mauricio Gonzatto", "49-99975-8575");
+}catch(Exception ex)
+{
+    Console.WriteLine($"Erro ao tentar incluir o valor repetido.");
+    Console.WriteLine(ex.Message);
+    throw ex;
+}
+
+//Percorrendo itens da HashTable
+Console.WriteLine("Agenda Telefônica");
+if(phoneBook.Count == 0)
+{
+    Console.WriteLine("A agenda está vazia.");
+}
+else
+{
+    foreach(DictionaryEntry entry in phoneBook)
+    {
+        Console.WriteLine($"{entry.Key}:{entry.Value}");     //Uma instrução não precisa de chaves
+    }
+}
