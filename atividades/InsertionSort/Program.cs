7395ebc14ee50e435c232efbe00b00c31acba78a
Thiago Seiji Takizawa <sanseiseiji@gmail.com>
2024-10-24 18:03:37 -0300

    Add files via upload
    InsertionSort

---
+++ atividades/InsertionSort/Program.cs
@@ -0,0 +1,9 @@
+﻿﻿using System.Reflection.Metadata;
+using AtvOrdenacao;
+
+string[] names = new string[3]{"Thiago", "Seiji", "Takizawa"};
+
+AtvOrdenacao.InsertionSort.Sort(names);
+
+foreach(string name in names)
+    System.Console.WriteLine(name);
