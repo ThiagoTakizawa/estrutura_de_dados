92ae0229f8884845dfb62e7d62280e36ea9c1e5a
Thiago Seiji Takizawa <433989@unoesc.lan>
2024-10-23 21:49:48 -0300

    Arvores

---
+++ atividades/Company/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Company;
+
+Tree <Person> company = new Tree<Person> ();
+company.Root = new TreeNode<Person> ()
+{
+    Data = new Person (100, "Marcin Jamro", "CEO"),
+    Parent = null
+};
+company.Root.Children = new List<TreeNode<Person>> ()
+{
+    Data = new Person (1, "John Smith", "Head of Development"),
+    Parent = company.Root
+}
